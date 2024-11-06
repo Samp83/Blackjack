39281def803213b2db6d0392327817c2d389c6b4
Samp83 <sami.paris@supdevinci-edu.fr>
2024-11-06 14:58:40 +0100

    merge

--- Backend/game/admin.py
+++ Backend/game/admin.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from game.models import Game, Player
 
 # Register your models here.
 admin.site.register(Game)
-=======
-from django.contrib import admin
-from game.models import Game, Player
-
-# Register your models here.
-admin.site.register(Game)
->>>>>>> Sami
 admin.site.register(Player)
