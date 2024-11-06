6785b45a7ed0f4c724b689b9e06dbf61ea413d83
Owen <owenouiluobe@gmail.com>
2024-11-06 15:07:23 +0100

    Modif du fichier App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,18 +4,16 @@ import './App.css'
 import PlayerForm from './components/PlayerForm';
 import GamePage from './components/GamePage';
 
-    function App() {
+function App() {
       const [players, setPlayers] = useState([]);
       const [diceCount, setDiceCount ] = useState(1);
     
       return (
           <Routes>
             <Route path="/" element={<PlayerForm setPlayers={setPlayers} setDiceCount={setDiceCount} />} />
-            <Route path="/game" element={<GamePage players={players} diceCount={diceCount} />} />
-
+            <Route path="/game:id" element={<GamePage players={players} diceCount={diceCount} />} />
           </Routes>
-
-          
       );
     }
+
 export default App
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    //const { createGame } = useCreateGame(); 
+
+    return (
+    <>
+    <h1>Blackjack Simplifié</h1>;
+    </>
+    );
+}
